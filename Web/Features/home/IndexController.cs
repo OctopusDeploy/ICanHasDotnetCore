7603ef68d4460d8594d1972e394deb87475ce336
Robert Wagner <robert.wagner@octopus.com>
2016-07-07 10:17:15 +1000

    Fixed up NotFound paths

--- Web/Features/home/IndexController.cs
+++ Web/Features/home/IndexController.cs
@@ -1,13 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ICanHasDotnetCore.Web.Controllers
 {
     [Route("{*url}")]
     public class IndexController : Controller
     {
+        private static readonly string[] NotFoundPaths = new[] { "api", "images", "app" };
         public ActionResult Get(string url)
         {
-            if(url == null  || url.StartsWith("api") || url.StartsWith("images") || url.StartsWith("app"))
+            if (url != null && NotFoundPaths.Any(url.StartsWith))
                 return NotFound();
 
             return File("~/index.html", "text/html");

5265c89f5553c146664b3a46de6544518885a5f8
Robert Wagner <robert.wagner@octopus.com>
2016-07-12 09:16:05 +1000

    Added ExternalLink directive

---
+++ Web/Features/layout/ExternalLink.ts
@@ -0,0 +1,19 @@
+﻿module ICanHasDotnetCore.Result.PackageResultBox {
+    app.directive("externalLink", ($state, $rootScope: ng.IRootScopeService) => {
+
+        return <ng.IDirective> {
+            restrict: "E",
+            template: "<a href=\"{{href}}\" target=\"_blank\"><md-icon class=\"external-link\">launch</md-icon></a>",
+            replace: true,
+            scope: {
+                href: "@"
+            },
+            transclude: true,
+            link: (scope: ng.IScope, element, attr, ctrl, transclude) => {
+                element.prepend(transclude());
+            }
+        }
+    }
+    );
+
+}

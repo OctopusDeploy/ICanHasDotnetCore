f9755c27e60e2bc0da0c99e7ded0c1b8414ccbf7
Henrik Andersson <henrik.a.andersson@outlook.com>
2017-07-28 16:27:19 +1000

    Fix scenario where old format csproj files exists in the folder/repo being scanned

--- source/Magic/SourcePackageFileReaders/CsProjReader.cs
+++ source/Magic/SourcePackageFileReaders/CsProjReader.cs
@@ -14,9 +14,14 @@ namespace ICanHasDotnetCore.SourcePackageFileReaders
         {
             using (var ms = new MemoryStream(contents))
             using (var sr = new StreamReader(ms))
+            using(var xr = XmlReader.Create(sr))
             {
-                var project = (Project) new XmlSerializer(typeof(Project)).Deserialize(sr);
-                return project.ItemGroups.Where(g => g.Packages.Any()).SelectMany(g => g.Packages).Select(p => p.Id).ToArray();
+                var xs = new XmlSerializer(typeof(Project));
+                if (!xs.CanDeserialize(xr)) return new string[0];
+
+                var project = (Project)new XmlSerializer(typeof(Project)).Deserialize(xr);
+                return project.ItemGroups.Where(g => g.Packages.Any()).SelectMany(g => g.Packages).Select(p => p.Id)
+                    .ToArray();
             }
         }
     }
